964006e4e26618853248c960f8c181d964a2be60
Michel Ferreira <michel.ferreira.melo@gmail.com>
2023-04-21 22:15:34 -0300

    fix: consertando o rodape

--- src/components/Rodape/index.jsx
+++ src/components/Rodape/index.jsx
@@ -1,9 +1,19 @@
 import React from "react";
+import { FaGithub, FaLinkedin } from 'react-icons/fa';
 import './rodape.css'
 
 const Rodape = () => {
   return(
-    <h1 className="rodape">Rodape</h1>
+    <div className="rodape">
+      <p>
+        Desenvolvipo por <strong>Michel Ferreira</strong>
+      </p>
+
+      <div className="social_media">
+        <a href="https://github.com/Miicchhel" target="_blanck"><FaGithub /></a>
+        <a href="https://www.linkedin.com/in/michel-ferreira-dev/" target="_blanck"><FaLinkedin /></a>
+      </div>
+    </div>
   )
 }
 
